944f891044c094efdc7f9084e74435506968b25a
Tim Renouf <tpr.ably@botech.co.uk>
2016-10-28 17:32:47 +0100

    wip in-place reauth tests
    Test for RTC8a2 is incomplete and the functionality does not seem to
    work anyway. So the test reauth_downgrade is disabled.

--- lib/src/test/java/io/ably/lib/test/realtime/RealtimeReauthTest.java
+++ lib/src/test/java/io/ably/lib/test/realtime/RealtimeReauthTest.java
@@ -28,6 +28,9 @@ public class RealtimeReauthTest {
 
 	/**
 	 * RTC8a: In-place reauthorization on a connected connection.
+	 * RTC8a1: A test should exist that performs an upgrade of
+	 * capabilities without any loss of continuity or connectivity
+	 * during the upgrade process.
 	 */
 	@Test
 	public void reauth_tokenDetails() {
@@ -76,10 +79,6 @@ public class RealtimeReauthTest {
 			assertEquals("Verify error code 40160 (channel is denied access)", error.code, 40160);
 			System.out.println("can't attach");
 
-			/* RTC8a1: A test should exist that performs an upgrade of
-			 * capabilities without any loss of continuity or connectivity
-			 * during the upgrade process. */
-
 			/* get second token */
 			tokenParams = new Auth.TokenParams();
 			capability = new Capability();
@@ -113,30 +112,97 @@ public class RealtimeReauthTest {
 			/* Verify that the connection never disconnected (0.9 in-place authorization) */
 			assertTrue("Expected in-place authorization", connectionWaiter.getCount(ConnectionState.connecting) == 0);
 
-			/* RTC8a2: Another test should exist where the capabilities are
-			 * downgraded resulting in Ably sending an ERROR ProtocolMessage
-			 * with a channel property, causing the channel to enter the FAILED
-			 * state. That test must assert that the channel becomes failed
-			 * soon after the token update and the reason is included in the
-			 * channel state change event. */
+		} catch (AblyException e) {
+			e.printStackTrace();
+			fail();
+		}
+	}
+
+	/**
+	/* RTC8a2: Another test should exist where the capabilities are
+	 * downgraded resulting in Ably sending an ERROR ProtocolMessage
+	 * with a channel property, causing the channel to enter the FAILED
+	 * state. That test must assert that the channel becomes failed
+	 * soon after the token update and the reason is included in the
+	 * channel state change event.
+	 *
+	 * DISABLED: see comment at the bottom of the test.
+	 */
+	/*@Test*/
+	public void reauth_downgrade() {
+		String wrongChannel = "wrongchannel";
+		String rightChannel = "rightchannel";
+		String testClientId = "testClientId";
+
+		try {
+			/* init ably for token */
+			final Setup.TestVars optsTestVars = Setup.getTestVars();
+			ClientOptions optsForToken = optsTestVars.createOptions(optsTestVars.keys[0].keyStr);
+			final AblyRest ablyForToken = new AblyRest(optsForToken);
+			System.out.println("done init ably for token");
+
+			/* get first (good) token */
+			Auth.TokenParams tokenParams = new Auth.TokenParams();
+			Capability capability = new Capability();
+			capability.addResource(wrongChannel, "*");
+			capability.addResource(rightChannel, "*");
+			tokenParams.capability = capability.toString();
+			tokenParams.clientId = testClientId;
+			Auth.TokenDetails firstToken = ablyForToken.auth.requestToken(tokenParams, null);
+			System.out.println("done get first token");
+			assertNotNull("Expected token value", firstToken.token);
+
+			/* create ably realtime with tokenDetails and clientId */
+			final Setup.TestVars testVars = Setup.getTestVars();
+			ClientOptions opts = testVars.createOptions();
+			opts.clientId = testClientId;
+			opts.tokenDetails = firstToken;
+			AblyRealtime ablyRealtime = new AblyRealtime(opts);
+			System.out.println("done create ably");
+
+			/* wait for connected state */
+			Helpers.ConnectionWaiter connectionWaiter = new Helpers.ConnectionWaiter(ablyRealtime.connection);
+			connectionWaiter.waitFor(ConnectionState.connected);
+			assertEquals("Verify connected state is reached", ConnectionState.connected, ablyRealtime.connection.state);
+			System.out.println("connected");
+
+			/* create a channel and check attached */
+			Channel channel = ablyRealtime.channels.get(rightChannel);
+			Helpers.CompletionWaiter waiter = new Helpers.CompletionWaiter();
+			channel.attach(waiter);
+			waiter = new Helpers.CompletionWaiter();
+			System.out.println("attaching");
+			channel.attach(waiter);
+			/* verify onSuccess callback gets called */
+			waiter.waitFor();
+			System.out.println("waited for attach");
+			assertThat(waiter.success, is(true));
+
+			/* get second (bad) token */
+			tokenParams = new Auth.TokenParams();
+			capability = new Capability();
+			capability.addResource(wrongChannel, "*");
+			tokenParams.capability = capability.toString();
+			tokenParams.clientId = testClientId;
+			System.out.println("got second token");
+			Auth.TokenDetails secondToken = ablyForToken.auth.requestToken(tokenParams, null);
+			assertNotNull("Expected token value", secondToken.token);
 
 			/* reauthorize */
-			System.out.println("Switching back to first token");
 			connectionWaiter.reset();
-			authOptions = new Auth.AuthOptions();
+			Auth.AuthOptions authOptions = new Auth.AuthOptions();
 			authOptions.key = optsTestVars.keys[0].keyStr;
-			authOptions.tokenDetails = firstToken;
-			reauthTokenDetails = ablyRealtime.auth.authorize(null, authOptions);
+			authOptions.tokenDetails = secondToken;
+			Auth.TokenDetails reauthTokenDetails = ablyRealtime.auth.authorize(null, authOptions);
 			assertNotNull("Expected token value", reauthTokenDetails.token);
-			System.out.println("done reauthorize 2");
+			System.out.println("done reauthorize");
 
-			/* Sleep to allow for server error message. */
-			try {
-				Thread.sleep(4000);
-			} catch (Exception e) {
-			}
-
-
+			/* Here we need to check that the channel moves to a failed state.
+			 * But:
+			 * 1. I haven't worked out how to do that yet.
+			 * 2. The server does not seem to send an error message for the
+			 *    channel on reauth anyway.
+			 */
 
 		} catch (AblyException e) {
 			e.printStackTrace();
